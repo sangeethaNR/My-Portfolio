c563f611b06dd8c8368a1ac049700de6fdf5fe22
sangeetha <sangeethajadhav123@gmail.com>
2021-06-18 20:54:31 -0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// function to skew the image
+
+function skew_fun()
+{
+   
+document.getElementById("giphy_cat").style.transform="skew(20deg, 10deg)";
+}
+
+//function to do scale,skew,rotate,translate the image 
+
+function transform_fun(){
+    document.getElementById("giphy_cat").style.transform=" matrix(1, 0, 0.5, 1, 150, 0)";
+}
+
+//function to rotate the image
+function rotate_fun(){
+    document.getElementById("giphy_cat").style.transform="rotate(-90deg)";
+}
