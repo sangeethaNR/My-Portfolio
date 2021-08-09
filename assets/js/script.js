530fdd64708131e6404e6e6902425e4b6e7ceadd
sangeethaNR <sangeethajadhav123@gmail.com>
2021-08-09 14:54:35 -0700

    added modal for the project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,46 @@
-// function to skew the image
 
-function skew_fun()
-{
+
+  $('#petfinder_id').on('click', function () {
+    $('#petfinder_modal').modal('show');
+
+
+});
+$('#weatherDashboard_id').on('click', function () {
+    $('#weatherDashboard_modal').modal('show');
+
+
+});
+
+$('#quiz_id').on('click', function () {
+    $('#quiz_modal').modal('show');
+
+
+});
+
+
+$('#petfinder_github').on('click', function () {
+    $('#quiz_modal').modal('show');
+
+
+});
+
+
+
+// // function to skew the image
+
+// function skew_fun()
+// {
    
-document.getElementById("giphy_cat").style.transform="skew(20deg, 10deg)";
-}
+// document.getElementById("giphy_cat").style.transform="skew(20deg, 10deg)";
+// }
 
-//function to do scale,skew,rotate,translate the image 
+// //function to do scale,skew,rotate,translate the image 
 
-function transform_fun(){
-    document.getElementById("giphy_cat").style.transform=" matrix(1, 0, 0.5, 1, 150, 0)";
-}
+// function transform_fun(){
+//     document.getElementById("giphy_cat").style.transform=" matrix(1, 0, 0.5, 1, 150, 0)";
+// }
 
-//function to rotate the image
-function rotate_fun(){
-    document.getElementById("giphy_cat").style.transform="rotate(-90deg)";
-}
+// //function to rotate the image
+// function rotate_fun(){
+//     document.getElementById("giphy_cat").style.transform="rotate(-90deg)";
+// }
